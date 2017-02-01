8e363ff9bfb5526d81850f906cd0c85059c9a862
JamesImpression <james.hall@impression.co.uk>
2017-02-01 12:29:33 +0000

    Update setup.rb
    separate out ssh-keyscan

--- wordpress/recipes/setup.rb
+++ wordpress/recipes/setup.rb
@@ -59,8 +59,12 @@ if !Dir.exists?("#{healthcheck_root}")
     action :install
   end
 
-  execute "ssh-keyscan" do
-    command "ssh-keyscan gitlab.com >> ~/.ssh/known_hosts; ssh-keyscan github.com >> ~/.ssh/known_hosts"
+  execute "ssh-keyscan-gitlab" do
+    command "ssh-keyscan gitlab.com >> ~/.ssh/known_hosts"
+  end
+  
+  execute "ssh-keyscan-github" do
+    command "ssh-keyscan github.com >> ~/.ssh/known_hosts"
   end
   
   git "/tmp" do 
