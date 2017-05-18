9ddd7e2fe8cb6b66e9ea9707fca9e15f11715dc2
JamesImpression <james.hall@impression.co.uk>
2017-05-18 14:43:52 +0100

    Update metadata.rb
    Version 8.1 Bump

--- wordpress/metadata.rb
+++ wordpress/metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "hello@pixels.fi"
 license          "All rights reserved"
 description      "Deploy WordPress from Github"
 long_description "Deploy WordPress from Github"
-version          "0.8.0"
+version          "0.8.1"
 
