ce0a9ff5a1fa362d43118e9877c9eb5fa2374725
JamesImpression <james.hall@impression.co.uk>
2017-07-27 14:16:36 +0100

    Update deploy.rb

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -6,8 +6,9 @@ search("aws_opsworks_app").each do |app|
 
     db = node['deploy']['wp']['database']
     domains = app['domains'].join(" ")
+    domains = domains.pop()
     domains_cert = app['domains'].join(" -d ")
-    wp_home =  app['environment']['WP_HOME'];
+    wp_home =  app['environment']['WP_HOME']
     if app['environment']['MULTISITE']
       site_url = wp_home
     else
