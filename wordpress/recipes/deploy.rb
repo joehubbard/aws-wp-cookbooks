5b6e55d38203993385c1302531920854b2d9b570
JamesImpression <james.hall@impression.co.uk>
2017-08-17 11:33:39 +0100

    Added in Mailchimp env

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -101,7 +101,9 @@ search("aws_opsworks_app").each do |app|
         :ilab_aws_s3_cache_control => "#{app['environment']['ILAB_AWS_S3_CACHE_CONTROL']}",
         :ilab_media_imgix_enabled => "#{app['environment']['ILAB_MEDIA_IMGIX_ENABLED']}",
         :ilab_aws_s3_cdn_base => "#{app['environment']['ILAB_AWS_S3_CDN_BASE']}",
-        :gmaps_api_key => "#{app['environment']['GMAPS_API_KEY']}"
+        :gmaps_api_key => "#{app['environment']['GMAPS_API_KEY']}",
+        :mailchimp_api_key => "#{app['enviroment']['MAILCHIMP_API_KEY']}",
+        :mailchimp_list_id => "#{app['enviroment']['MAILCHIMP_LIST_ID']}"
       )
     end
 
