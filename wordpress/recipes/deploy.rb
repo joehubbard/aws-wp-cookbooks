6e8ae920a842e268ebb14b43e817415cbff8a3ea
James H-Hall <james.hall@impression.co.uk>
2018-04-27 16:18:23 +0100

    Changed in deploy as well

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -180,10 +180,10 @@ search("aws_opsworks_app").each do |app|
         group "root"
         mode "644"
         variables(
-            :wp_home => "#{wp_home}",
-        }
+            :wp_home => "#{wp_home}"
+        )
         only_if do
-            File.file?("/etc/cron.hourly/wpcron")
+            !File.file?("/etc/cron.hourly/wpcron")
         end
     end
     
